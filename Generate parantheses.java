da5d2f4e08c58e7aa617a508eb5491f881b7aee5
saisusmitha848-sys <saisusmitha848@gmail.com>
2026-09-03 22:04:06 +0530

    sorting

---
+++ Generate parantheses.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        
+        backtrack(result, "", 0, 0, n);
+        
+        return result;
+    }
+
+    private void backtrack(List<String> result, String current,
+                            int open, int close, int n) {
+
+        // If the string contains 2*n brackets, it's complete
+        if (current.length() == 2 * n) {
+            result.add(current);
+            return;
+        }
+
+        // Add opening bracket if we still have some left
+        if (open < n) {
+            backtrack(result, current + "(", open + 1, close, n);
+        }
+
+        // Add closing bracket only when it is valid
+        if (close < open) {
+            backtrack(result, current + ")", open, close + 1, n);
+        }
+    }
+}
+
